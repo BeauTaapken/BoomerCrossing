4416ceb9154037fd3de9550a2fcf3e8c7797674b
Tomdatbenik <tomvankaathoven@gmail.com>
2020-05-28 12:11:34 +0200

    Inventory system added

---
+++ BoomerCrossing/Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+
+public class Inventory: ScriptableObject
+{
+    public List<InventorySlot> slots = new List<InventorySlot>();
+
+    public int size;
+
+    public InventoryState tryAddItem(Item item)
+    {
+        InventorySlot exsistingSlot = getSlotByItem(item);
+        if(exsistingSlot != null)
+        {
+            exsistingSlot.amount += 1;
+            Debug.Log("Slot that holds item: " + item.name + " holds " + exsistingSlot.amount + " " + item.name);
+            return InventoryState.ADDEDSUCCESFULL;
+        }
+        else
+        {
+            if (slots.Count < size)
+            {
+                slots.Add(new InventorySlot(item));
+                return InventoryState.ADDEDSUCCESFULL;
+            }
+            else
+            {
+                return InventoryState.FULL;
+            }
+        }       
+    }
+
+    public bool tryRemoveItem(Item item)
+    {
+        InventorySlot slot = getSlotByItem(item);
+
+        if(slot!= null)
+        {
+            slot.amount--;
+
+            if (slot.amount <= 0)
+            {
+                slots.Remove(slot);
+            }
+
+            Debug.Log("Slot amount left: " + slot.amount);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private InventorySlot getSlotByItem(Item item)
+    {
+        return slots.Find(I => I.item.name == item.name);
+    }
+}
---
+++ BoomerCrossing/Assets/Scripts/Inventory/InventoryManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public Inventory inventory;
+
+    public void AddItem(Item item)
+    {
+        InventoryState state = inventory.tryAddItem(item);
+
+        if (state == InventoryState.ADDEDSUCCESFULL)
+        {
+            Debug.Log("Item added to invenotry");
+        }
+        else if(state == InventoryState.FULL)
+        {
+            Debug.Log("Inventory full");
+        }
+
+        Debug.Log(inventory.slots.Count);
+    }
+
+    public void RemoveItem(Item item)
+    {
+        inventory.tryRemoveItem(item);
+    }
+}
