9614364972afac771ea497002ca80a2b11ac81aa
BeauTaapken <beautaapken@me.com>
2020-06-04 10:41:10 +0200

    * now able to place an item on your location that also moves with the planet when walking

---
+++ BoomerCrossing/Assets/Scripts/ItemPlacement/PlaceItem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceItem : MonoBehaviour
+{
+    public GameObject planetParts;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlaceItemOnGrid(GameObject itemToPlace)
+    {
+        GameObject placedItem = Instantiate(itemToPlace, new Vector3(player.transform.position.x, 26.0f, 0.0f), Quaternion.identity);
+        placedItem.transform.parent = planetParts.transform;
+        Debug.Log("Placed item on player location");
+    }
+}
